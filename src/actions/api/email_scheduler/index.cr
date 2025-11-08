91e08063cc72784354d11b42ed3fdeccbcb389fc
mike belanger <mikejamesbelanger@gmail.com>
2025-11-08 12:21:36 -0500

    Ensure no new splits get made if there's already a split available

--- src/actions/api/email_scheduler/index.cr
+++ src/actions/api/email_scheduler/index.cr
@@ -11,36 +11,55 @@ class Api::SendEmail < ApiAction
     if first_user.nil? || second_user.nil?
       json({error: "Users not found"})
     else
-      first_user_purchases = PurchaseQuery.new.users_purchases_for_month(user: first_user, from: from, to: to)
-      second_user_purchases = PurchaseQuery.new.users_purchases_for_month(user: second_user, from: from, to: to)
+      # First, check to ensure there isn't a split already for this month
+      existing_split = SplitQuery.new
+        .first_user_id(first_user.id)
+        .second_user_id(second_user.id)
+        .start_day(from)
+        .end_day(to)
+        .first
 
-      first_total = first_user_purchases.sum(&.dollars)
-      second_total = second_user_purchases.sum(&.dollars)
+      if existing_split
+        bill = BillEmail.new(
+          recipient: first_user,
+          split: existing_split
+        )
 
-      total = first_total + second_total
+        bill.deliver
 
-      SaveSplit.create(
-        first_user_id: first_user.id,
-        second_user_id: second_user.id,
-        start_day: from,
-        end_day: to,
-        total_amount: total.to_i32,
-        paid: false,
-        first_user_amount: first_total.to_i32,
-        second_user_amount: second_total.to_i32,
-        bill_sent_amount: 0,
-      ) do |operation, split|
-        if split
-          # Split created successfully
-          bill = BillEmail.new(
-            recipient: first_user,
-            split: split
-          )
+        json({message: "Email sent (again): #{existing_split.outcome}"})
+      else
+        first_user_purchases = PurchaseQuery.new.users_purchases_for_month(user: first_user, from: from, to: to)
+        second_user_purchases = PurchaseQuery.new.users_purchases_for_month(user: second_user, from: from, to: to)
 
-          bill.deliver
-          json({message: "Email sent: #{split.outcome}"})
-        else
-          json({message: "Message not sent successfully"})
+        first_total = first_user_purchases.sum(&.dollars)
+        second_total = second_user_purchases.sum(&.dollars)
+
+        total = first_total + second_total
+
+        SaveSplit.create(
+          first_user_id: first_user.id,
+          second_user_id: second_user.id,
+          start_day: from,
+          end_day: to,
+          total_amount: total.to_i32,
+          paid: false,
+          first_user_amount: first_total.to_i32,
+          second_user_amount: second_total.to_i32,
+          bill_sent_amount: 0,
+        ) do |operation, split|
+          if split
+            # Split created successfully
+            bill = BillEmail.new(
+              recipient: first_user,
+              split: split
+            )
+
+            bill.deliver
+            json({message: "Email sent: #{split.outcome}"})
+          else
+            json({message: "Message not sent successfully"})
+          end
         end
       end
     end
