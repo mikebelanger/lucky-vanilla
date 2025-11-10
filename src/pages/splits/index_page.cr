b93841f2f4b08b3fce13ab0c04823aa6645cb5c4
mike belanger <mikejamesbelanger@gmail.com>
2025-11-10 07:31:18 -0500

    style split based on if its been paid or not

--- src/pages/splits/index_page.cr
+++ src/pages/splits/index_page.cr
@@ -9,6 +9,7 @@ class Splits::IndexPage < MainLayout
           th "Total"
           th "My share"
           th "Number of bills sent"
+          th "Paid on"
           th "Start"
           th "End"
           th "Actions"
@@ -16,7 +17,14 @@ class Splits::IndexPage < MainLayout
       end
       tbody do
         splits.each do |split|
-          tr do
+          paid_on = split.paid_on
+
+          row_class_name = if paid_on
+                             "paid-bg"
+                           else
+                             ""
+                           end
+          tr(class: row_class_name) do
             td do
               text "$#{split.total_amount}"
             end
@@ -29,6 +37,13 @@ class Splits::IndexPage < MainLayout
             end
             td do
               text split.bill_sent_amount
+            end
+            td do
+              if paid_on
+                text "#{paid_on.date_without_time}, #{paid_on.year}"
+              else
+                text "Not paid"
+              end
             end
             td do
               text "#{split.start_day.date_without_time}, #{split.start_day.year}"
