4def09094749dc7158800da7f62b0d69d3408669
mike belanger <mikejamesbelanger@gmail.com>
2025-12-16 07:35:00 -0500

    Simplify periodic split reminder logic to one upsert statement

--- src/actions/api/email_scheduler/index.cr
+++ src/actions/api/email_scheduler/index.cr
@@ -11,58 +11,36 @@ class Api::SendEmail < ApiAction
     if first_user.nil? || second_user.nil?
       json({error: "Users not found"})
     else
-      # Query all splits for the current month
-      this_months_splits = SplitQuery.new
-        .first_user_id(first_user.id)
-        .second_user_id(second_user.id)
-        .start_day(from)
-        .end_day(to)
+      first_user_purchases = PurchaseQuery.new.users_purchases_for_month(user: first_user, from: from, to: to)
+      second_user_purchases = PurchaseQuery.new.users_purchases_for_month(user: second_user, from: from, to: to)
 
-      # Loop all the splits, and email out a bill for any unpaid split
-      this_months_splits.each do |split|
-        if split.paid_on.nil?
+      first_total = first_user_purchases.sum(&.dollars)
+      second_total = second_user_purchases.sum(&.dollars)
+
+      total = first_total + second_total
+
+      SaveSplit.upsert(
+        first_user_id: first_user.id,
+        second_user_id: second_user.id,
+        paid: false,
+        start_day: from,
+        end_day: to,
+        first_user_amount: first_total.to_i32,
+        second_user_amount: second_total.to_i32,
+        total_amount: total.to_i32
+      ) do |operation, split|
+        if operation.created?
+          puts "This one is new"
+        else
+          puts "This one is an update"
+        end
+
+        if split
           bill = BillEmail.new(
             recipient: first_user,
             split: split
           )
           bill.deliver
-          json({message: "Email sent (again): #{split.outcome}"})
-        end
-      end
-
-      # If there wasn't any splits for this month yet, derive one from this month's purchases
-      if this_months_splits.size == 0
-        first_user_purchases = PurchaseQuery.new.users_purchases_for_month(user: first_user, from: from, to: to)
-        second_user_purchases = PurchaseQuery.new.users_purchases_for_month(user: second_user, from: from, to: to)
-
-        first_total = first_user_purchases.sum(&.dollars)
-        second_total = second_user_purchases.sum(&.dollars)
-
-        total = first_total + second_total
-
-        SaveSplit.create(
-          first_user_id: first_user.id,
-          second_user_id: second_user.id,
-          start_day: from,
-          end_day: to,
-          total_amount: total.to_i32,
-          paid: false,
-          first_user_amount: first_total.to_i32,
-          second_user_amount: second_total.to_i32,
-          bill_sent_amount: 0,
-        ) do |operation, split|
-          if split
-            # Split created successfully
-            bill = BillEmail.new(
-              recipient: first_user,
-              split: split
-            )
-
-            bill.deliver
-            json({message: "Email sent: #{split.outcome}"})
-          else
-            json({message: "Message not sent successfully"})
-          end
         end
       end
       json({message: "Mail sending finished"})
--- src/operations/save_split.cr
+++ src/operations/save_split.cr
@@ -5,6 +5,8 @@ class SaveSplit < Split::SaveOperation
   # permit_columns total_amount, paid, paid_on, first_user, second_user
   permit_columns start_day, end_day, total_amount, paid, paid_on, first_user_id, second_user_id, first_user_amount, second_user_amount
 
+  upsert_lookup_columns :first_user_id, :second_user_id, :paid, :start_day, :end_day
+
   def mark_sent
     @bill_sent_amount += 1
   end
