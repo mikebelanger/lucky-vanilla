11960d12b4c362afc1c672ed0d87c600c1353d58
mike belanger <mikejamesbelanger@gmail.com>
2025-12-20 13:54:41 -0500

    Create script which wraps app in podman pods

--- create.sh
+++ create.sh
@@ -1,21 +1,21 @@
-podman network create vanilla_net
+podman pod create \
+--replace \
+-p 3000:3000 \
+--name vanilla_app
 
-# podman pod create vanilla
-
-podman run --replace \
---name=vanilla_postgres \
---network=vanilla_net \
+podman create \
+--replace \
+--name vanilla_postgres \
 -v postgres_data:/var/lib/postgresql/data \
 -e POSTGRES_USER=lucky \
 -e POSTGRES_PASSWORD=password \
 -e POSTGRES_DB=lucky \
--p 6543:5432 \
---pod vanilla \
--d \
+--pod vanilla_app \
 postgres:14-alpine
 
-podman run --replace \
---name=vanilla_app \
+podman create \
+--replace \
+--name vanilla_api \
 --requires=vanilla_postgres \
 -v /home/mike/.local/share/containers/storage/volumes/vanilla-app_postgres_data/_data \
 -v .:/app \
@@ -23,21 +23,15 @@ podman run --replace \
 -v shards_lib:/app/lib \
 -v app_bin:/app/bin \
 -v build_cache:/root/.cache \
--p 3000:3000 \
--p 3001:3001 \
 --entrypoint=docker/dev_entrypoint.sh \
 -e DATABASE_URL=postgres://lucky:password@vanilla_postgres:5432/lucky \
 -e DEV_HOST="0.0.0.0" \
---network=vanilla_net \
---pod vanilla \
+--pod vanilla_app \
 localhost/vanilla-app_lucky:latest
 
-podman run --replace \
--d \
---name=scheduler \
---requires=vanilla_app \
---network=vanilla_net \
+podman build -f docker/scheduler.dockerfile --no-cache -t scheduler
+podman create \
+--pod vanilla_app \
+--requires=vanilla_api \
 -e HOST_URL=vanilla_app \
--p 6789:6789 \
---pod vanilla \
-localhost/scheduler:latest
+scheduler
