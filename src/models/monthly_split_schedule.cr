ea18f19d9ae9d09ac8e2ef8e0e0994eeb46faf42
mike belanger <mikejamesbelanger@gmail.com>
2026-01-01 16:03:24 -0500

    Minor code formatting

--- src/models/monthly_split_schedule.cr
+++ src/models/monthly_split_schedule.cr
@@ -4,10 +4,4 @@ class MonthlySplitSchedule < BaseModel
     belongs_to second_user : User
     has_many splits : Split?
   end
-  # def splits
-  #   unless self.splits.nil?
-  #     self.splits
-  #   end
-  #   [] of Split
-  # end
 end
--- src/queries/split_query.cr
+++ src/queries/split_query.cr
@@ -1,8 +1,2 @@
 class SplitQuery < Split::BaseQuery
-  # def current_user_splits(user : User, schedule : MonthlySplitSchedule)
-  #   [] of Split
-  # end
-  def splits
-  end
 end
-# self.monthly_split_schedule_id(schedule.id).first_user_id(user.id).or(&.second_user_id(user.id)).to_a
