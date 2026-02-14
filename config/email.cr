28fa575392b075c70a9feaf1b2c313ea5afe085c
mike belanger <mikejamesbelanger@gmail.com>
2026-02-14 11:09:21 -0500

    uprev carbon version in mailersend adapter for compatibility

--- config/email.cr
+++ config/email.cr
@@ -1,4 +1,4 @@
-require "carbon_mailgun_adapter"
+require "carbon_mailersend_adapter"
 
 BaseEmail.configure do |settings|
   if LuckyEnv.production?
@@ -6,10 +6,8 @@ BaseEmail.configure do |settings|
     # If you do need emails, get a key from SendGrid and set an ENV variable
     # send_grid_key = send_grid_key_from_env
     # settings.adapter = Carbon::SendGridAdapter.new(api_key: send_grid_key)
-    mailgun_key = ENV["MAILGUN_API_KEY"]
-    mailgun_base_uri = ENV["MAILGUN_BASE_URI"]
-    mailgun_send_domain = ENV["MAILGUN_SEND_DOMAIN"]
-    settings.adapter = Carbon::MailgunAdapter.new(api_key: mailgun_key, base_uri: mailgun_base_uri, send_domain: mailgun_send_domain)
+    mailersend_key = ENV["MAILGUN_SEND_KEY"]
+    settings.adapter = Carbon::MailersendAdapter.new(api_key: mailersend_key)
   elsif LuckyEnv.development?
     settings.adapter = Carbon::DevAdapter.new(print_emails: true)
   else
