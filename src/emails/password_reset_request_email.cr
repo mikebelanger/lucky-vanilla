acfcd4fac605d5e5eb61e0533d7c18936501b6c1
mike belanger <mikejamesbelanger@gmail.com>
2026-02-21 10:48:52 -0500

    Define default sender

--- src/emails/password_reset_request_email.cr
+++ src/emails/password_reset_request_email.cr
@@ -7,7 +7,7 @@ class PasswordResetRequestEmail < BaseEmail
   end
 
   to @user
-  from "myapp@support.com" # or set a default in src/emails/base_email.cr
+  from "support@vanillasplit.com" # or set a default in src/emails/base_email.cr
   subject "Reset your password"
   templates html, text
 end
