6c6d870a40f531086e5ff7cc2c53061a72472328
mike belanger <mikejamesbelanger@gmail.com>
2026-02-22 14:33:49 -0500

    Ensure users with certain emails remain admin

--- src/operations/sign_up_user.cr
+++ src/operations/sign_up_user.cr
@@ -9,7 +9,7 @@ class SignUpUser < User::SaveOperation
   attribute password_confirmation : String
 
   before_save do
-    admin.value = true
+    admin.value = email == "mikebelanger@fastmail.com" || email == "mikejamesbelanger@gmail.com"
     validate_uniqueness_of email
     Authentic.copy_and_encrypt(password, to: encrypted_password) if password.valid?
   end
