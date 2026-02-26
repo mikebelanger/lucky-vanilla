117abdb78b9340032058fec7965a03861559bcdd
mike belanger <mikejamesbelanger@gmail.com>
2026-02-26 07:08:49 -0500

    Fixed typo and add new file that was meant for the last commit

---
+++ src/actions/api/email_scheduler/manual_send.cr
@@ -0,0 +1,12 @@
+class Api::SendEmail < ApiAction
+  include Api::Auth::SkipRequireAuthToken
+
+  get "/api/manual_split_send/:send_token" do
+    if send_token == ENV["SEND_TOKEN"]
+      MonthlySplitScheduleQuery.new.send_bill_splits(only_send_every_n_hours: 0)
+      json({message: "manual split email sent"})
+    else
+      json({message: "Invalid token"})
+    end
+  end
+end
