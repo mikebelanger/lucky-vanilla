7df9514b48a2a00a160da98b765574680a48890d
mike belanger <mikejamesbelanger@gmail.com>
2026-02-26 19:26:24 -0500

    Change time between email sends on bills

--- src/actions/api/email/send.cr
+++ src/actions/api/email/send.cr
@@ -4,7 +4,7 @@ class Api::Send < ApiAction
   get "/api/email/send/:send_token" do
     now = Time.local
     if now.end_of_month? && send_token == ENV["SEND_TOKEN"]
-      MonthlySplitScheduleQuery.new.send_bill_splits(only_send_every_n_hours: 2)
+      MonthlySplitScheduleQuery.new.send_bill_splits(only_send_every_n_hours: 24)
       json({message: "periodic email hit"})
     else
       json({message: "It's not that time yet"})
