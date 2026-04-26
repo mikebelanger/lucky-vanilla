2ba85e05f22853c69c068fd018e19a4bffb6dae9
mike belanger <mikejamesbelanger@gmail.com>
2026-04-26 10:34:08 -0400

    Reverse-sort purchases so most recent purchaess are at the top of the page

--- src/pages/purchases/index_page.cr
+++ src/pages/purchases/index_page.cr
@@ -9,28 +9,32 @@ class Purchases::IndexPage < MainLayout
   def render_purchases
     ul(class: "container") do
       purchases
+        .to_a
+        .sort! { |purchase_a, purchase_b| (purchase_b.date.year + purchase_b.date.month) <=> (purchase_a.date.year + purchase_a.date.month) }
         .group_by { |purchase| {year: purchase.date.year, month: purchase.date.month} }
         .each do |date, purchases|
           h4 do
             text "#{Month.new(date[:month]).name} #{date[:year]}"
           end
           hr
-          purchases.each do |purchase|
-            details(class: "purchase-item") do
-              summary(class: "outline", role: "button") do
-                span do
-                  strong do
-                    text "$#{purchase.dollars}"
+          purchases
+            .sort { |purchase_a, purchase_b| purchase_a.date.day <=> purchase_b.date.day }
+            .each do |purchase|
+              details(class: "purchase-item") do
+                summary(class: "outline", role: "button") do
+                  span do
+                    strong do
+                      text "$#{purchase.dollars}"
+                    end
+                    text " on #{purchase.date.date_without_time}"
                   end
-                  text " on #{purchase.date.date_without_time}"
                 end
+                if description = purchase.description
+                  span description
+                end
+                link "Edit Purchase", to: Purchases::Edit.with(purchase), resource_id: purchase.id, reload_element_id: "purchases"
               end
-              if description = purchase.description
-                span description
-              end
-              link "Edit Purchase", to: Purchases::Edit.with(purchase), resource_id: purchase.id, reload_element_id: "purchases"
             end
-          end
         end
     end
     div class: "add-split" do
