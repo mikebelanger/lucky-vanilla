52eb28cb1a9392c9e316b3b2e91c07d6f18cd125
mike belanger <mikejamesbelanger@gmail.com>
2026-07-01 12:59:08 -0400

    Remove unnecessary check for yarn

--- script/system_check.cr
+++ script/system_check.cr
@@ -9,11 +9,6 @@ require "./helpers/*"
 #   * Checking that elasticsearch, redis, or postgres is installed and/or booted
 #   * Note: Booting additional processes for things like mail, background jobs, etc...
 #     should go in your Procfile.dev.
-
-if command_not_found "yarn"
-  print_error "Yarn is not installed\n  See https://yarnpkg.com/lang/en/docs/install/ for install instructions."
-end
-
 # CUSTOM PRE-BOOT CHECKS
 # example:
 # if command_not_running "redis-cli", "ping"
