dd67ef7a3d1a7b168ef5fbfe8cb47e95a136eeec
mike belanger <mikejamesbelanger@gmail.com>
2026-07-12 14:42:17 -0400

    Add in proper podman pull commands for deploy, and change volume-mounted production data to be a persistentVolumeClaim to prepare for rootless podman, and to delegate permissions to podman

--- containers/deploy.sh
+++ containers/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-podman pull ghcr.io/me/vanilla_prod_api:latest
-podman pull ghcr.io/me/vanilla_prod_scheduler:latest
+podman pull ghcr.io/mikebelanger/lucky-vanilla/vanilla_prod_api:latest
+podman pull ghcr.io/mikebelanger/lucky-vanilla/vanilla_prod_scheduler:latest
 exec podman kube play --replace \
   --configmap ./prod/config.yml \
   --configmap ./prod/secrets.yml \
